b96a59c511cce026eaad21a247c6e2197a4dc062
Jason Ward <wmjasonward@gmail.com>
2018-05-30 21:22:22 -0500

    added tests for no postgres server and active client connections

--- __tests__/netstat.js
+++ __tests__/netstat.js
@@ -6,9 +6,26 @@ var fs = require('fs');
 var path = require('path');
 var exec = require('child_process').exec;
 
-var findpgsocket = require('../index.js');
+var findpgsockets = require('../index.js');
 
 var netstatFixturesDir = path.resolve(__dirname, '../__fixtures__/netstats/');
+
+test('it calls cb with Error of expected shape', function(done) {
+  exec.mockImplementationOnce(function(cmd, opts, cb) {
+    var e = Error('mock exec error');
+    cb(e, 'mock stdout content', 'mock stderr content');
+  });
+
+  findpgsockets(function(err, sockets) {
+    expect(err).toBeInstanceOf(Error);
+    expect(err).toMatchObject({
+      stdout: 'mock stdout content',
+      stderr: 'mock stderr content',
+    });
+    done();
+  });
+});
+
 
 /**
  * defines our test files (captured netstat outputs) and expected results
@@ -22,6 +39,22 @@ var tests = [
       '/var/run/postgresql/.s.PGSQL.5432',
       '/tmp/.s.PGSQL.5432',
     ],
+  },
+  {
+    /* test that we only get each socket file once even if it is in netstat -a results
+       multiple times due to active client connections
+     */
+    name: '"netstat -a" on fedora27 with established socket connection (psql)',
+    file: 'fedora27-002-positive',
+    results: [
+      '/var/run/postgresql/.s.PGSQL.5432',
+      '/tmp/.s.PGSQL.5432',
+    ],
+  },
+  {
+    name: '"netstat -a" on fedora27 without postgres running',
+    file: 'fedora27-001-negative',
+    results: [],
   },
   {
     name: '"netstat -a" on macosx with postgres listening on one socket',
@@ -45,10 +78,10 @@ for (var t = 0; t < tests.length; t++) {
         cb(null, fs.readFileSync(path.join(netstatFixturesDir, ct.file)).toString(), '');
       });
 
-      findpgsocket(function (err, sockets) {
+      findpgsockets(function (err, sockets) {
         expect(sockets.sort()).toEqual(ct.results.sort());
         done();
       });
-    }
+    };
   })(curtest));
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var exec = require('child_process').exec;
 
-var pgsql_socket_regex = /\/.*\.s.PGSQL\.\d{1,5}/g;
+var pgsql_sockets_regex = /\/.*\.s.PGSQL\.\d{1,5}/g;
 
 /**
  * Returns an array of socket files from the result of
@@ -11,28 +11,28 @@ var pgsql_socket_regex = /\/.*\.s.PGSQL\.\d{1,5}/g;
  * @param stdin
  * @returns {Array}
  */
-function extractSocketsFromNetstatResult(stdin) {
-  var paths = stdin.match(pgsql_socket_regex);
-  // remove dups but keep order (no indication that order matters)
+function extractSocketsFromNetstatResult(stdout) {
+  var paths = stdout.match(pgsql_sockets_regex);
+  // remove dups, but keep order (event though order doesn't matter for us)
   // https://stackoverflow.com/a/15868720/358977
-  return paths.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+  return paths ? paths.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]): [];
 }
 
 
 function getFromNetstat(cb) {
-  var m = exec('netstat -a', {}, function(err, stdin, stderr) {
+  exec('netstat -a', {}, function(err, stdout, stderr) {
     if (err) {
-      err.stdin = stdin;
+      err.stdout = stdout;
       err.stderr = stderr; // should probably just go on debug
       cb(err);
     } else {
-      cb(null, extractSocketsFromNetstatResult(stdin));
+      cb(null, extractSocketsFromNetstatResult(stdout));
     }
   });
 }
 
-// our 'findpgsocket' function
+// our 'findpgsockets' function
 // currently just uses getFromNetstat
 module.exports = function(cb) {
   getFromNetstat(cb);
-}
+};
